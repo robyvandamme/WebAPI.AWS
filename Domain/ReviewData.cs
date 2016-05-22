96e6927c3e164acd2f4381b57f949b718b40d3b7
Roby Van Damme <roby@robyvandamme.com>
2016-05-22 16:45:55 +0800

    Introduce Domain and data interface

---
+++ Domain/ReviewData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Domain
+{
+    public interface IReviewData
+    {
+        IEnumerable<Review> GetReviews();
+        Review GetReview();
+    }
+
+    public class ReviewData : IReviewData
+    {
+        public IEnumerable<Review> GetReviews()
+        {
+            return new List<Review>();
+        }
+
+        public Review GetReview()
+        {
+            return new BookReview();
+        }
+    }
+}
