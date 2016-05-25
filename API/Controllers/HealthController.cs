6980c0a9c8ed6fda9c8ab1be61a59fd575041080
Roby Van Damme <roby@robyvandamme.com>
2016-05-25 12:28:28 +0800

    Add Health endpoint for ELB

---
+++ API/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// Health endpoint for the load balancer
+    /// </summary>
+    public class HealthController : ApiController
+    {
+        public HttpStatusCode Get()
+        {
+            return HttpStatusCode.OK;
+        }
+    }
+}
